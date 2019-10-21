f8daa2cee4f59f4decdf4df5229b4c18bed47189
Jenna Dean <jenna.dean9@gmail.com>
2019-10-21 07:58:24 -0400

    final

--- script.js
+++ script.js
@@ -31,14 +31,15 @@ const tabLinks = document.querySelector("#tab-links")
 alderaan.addEventListener("click", function (e){
 	e.preventDefault();
 	body.classList.add("ald");
-	body.classList.remove("h", "cor", "bes", "kam")
+	body.classList.remove("h", "cor", "bes", "kam");
+	body.style.background.animation = "fadeIn 2s";
 	fetch(rooturl)
 		.then(res => res.json())
 		.then(res => {
 			console.log(res.results[0])
 			description.innerText = `Welcome to ${res.results[0].name}! 
 			${res.results[0].name} is ${res.results[0].diameter} kilometers in diameter, and has ${res.results[0].orbital_period} days in a year.
-			The planet ${res.results[0].name} is ${res.results[0].surface_water}% water, and the ${res.results[0].terrain} terrain populate ${res.results[0].population} beings.
+			The planet ${res.results[0].name} is ${res.results[0].surface_water}% water, and the terrain of ${res.results[0].terrain} inhabit ${res.results[0].population} beings.
 			Thanks for visiting and come back soon!`;
 		})
 		.catch(err=>{
@@ -55,7 +56,7 @@ coruscant.addEventListener("click", function (e){
 			console.log(res.results[7])
 			description.innerText = `Welcome to ${res.results[7].name}! 
 			${res.results[7].name} is ${res.results[7].diameter} kilometers in diameter, and has ${res.results[7].orbital_period} days in a year.
-			The planet ${res.results[7].name} is ${res.results[7].surface_water}% water, and the ${res.results[7].terrain} of the planet populate ${res.results[7].population} beings.
+			The planet ${res.results[7].name} is ${res.results[7].surface_water}% water, and the terrain of ${res.results[7].terrain} inhabit ${res.results[7].population} beings.
 			Thanks for visiting and come back soon!`;
 		})
 		.catch(err=>{
@@ -72,7 +73,7 @@ hoth.addEventListener("click", function (e){
 			console.log(res.results[2])
 			description.innerText = `Welcome to ${res.results[2].name}! 
 			${res.results[2].name} is ${res.results[2].diameter} kilometers in diameter, and has ${res.results[2].orbital_period} days in a year.
-			The planet ${res.results[2].name} is ${res.results[2].surface_water}% water, and the ${res.results[2].terrain} terrain populate ${res.results[2].population} beings.
+			The planet ${res.results[2].name} is ${res.results[2].surface_water}% water, and the terrain of ${res.results[2].terrain} inhabit ${res.results[2].population} beings.
 			Thanks for visiting and come back soon!`;
 		})
 		.catch(err=>{
@@ -89,7 +90,7 @@ bespin.addEventListener("click", function (e){
 			console.log(res.results[4])
 			description.innerText = `Welcome to ${res.results[4].name}! 
 			${res.results[4].name} is ${res.results[4].diameter} kilometers in diameter, and has ${res.results[4].orbital_period} days in a year.
-			The planet ${res.results[4].name} is ${res.results[4].surface_water}% water, and the ${res.results[4].terrain} terrain populates ${res.results[4].population} beings.
+			The planet ${res.results[4].name} is ${res.results[4].surface_water}% water, and the terrain of the ${res.results[4].terrain} inhabits ${res.results[4].population} beings.
 			Thanks for visiting and come back soon!`;
 		})
 		.catch(err=>{
@@ -106,7 +107,7 @@ kamino.addEventListener("click", function (e){
 			console.log(res.results[8])
 			description.innerText = `Welcome to ${res.results[8].name}! 
 			${res.results[8].name} is ${res.results[8].diameter} kilometers in diameter, and has ${res.results[8].orbital_period} days in a year.
-			The planet ${res.results[8].name} is ${res.results[8].surface_water}% water, and the ${res.results[8].terrain} terrain populates ${res.results[8].population} beings.
+			The planet ${res.results[8].name} is ${res.results[8].surface_water}% water, and the ${res.results[8].terrain} terrain inhabits ${res.results[8].population} beings.
 			Thanks for visiting and come back soon!`;
 		})
 		.catch(err=>{
