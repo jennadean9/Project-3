62de55a3c87380ff9c6646084d2233dfe8662e8d
Jenna Dean <jenna.dean9@gmail.com>
2019-12-05 15:24:18 -0500

    made into tabs, done

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const body = document.querySelector("body");
 const rooturl = "https://swapi.co/api/planets";
-const alderaan = document.querySelector(".alderaan");
-const coruscant = document.querySelector(".coruscant");
-const hoth = document.querySelector(".hoth");
-const bespin = document.querySelector(".bespin");
-const kamino = document.querySelector(".kamino");
+const alderaan = document.querySelector("#alderaan");
+const coruscant = document.querySelector("#coruscant");
+const hoth = document.querySelector("#hoth");
+const bespin = document.querySelector("#bespin");
+const kamino = document.querySelector("#kamino");
 const description = document.querySelector("p");
-const tabLinks = document.querySelector("#tab-links")
+const tabLinks = document.querySelector(".tab-links");
+const pills = document.querySelectorAll(".pill");
+const container = document.querySelector(".description-container");
 
 //code didn't work for one loop for all
 // tabLinks.addEventListener("click", function(e){
@@ -16,7 +18,7 @@ const tabLinks = document.querySelector("#tab-links")
 // 			.then(res=> res.json())
 // 			.then(res=> {
 // 				console.log(res.results[i])
-// 				description.innerText = `Welcome to ${res.results[i].name}! 
+// 				description.innerText = `Welcome to ${res.results[i].name}!
 // 				${res.results[i].name} is ${res.results[i].diameter} kilometers in diameter, and has ${res.results[i].orbital_period} days in a year.
 // 				The planet ${res.results[i].name} is ${res.results[i].surface_water}% water, and the ${res.results[i].terrain} terrain populate ${res.results[i].population} beings.
 // 				Thanks for visiting and come back soon!`;
@@ -27,89 +29,119 @@ const tabLinks = document.querySelector("#tab-links")
 // 	}
 // })
 
+alderaan.addEventListener("click", function(e) {
+  e.preventDefault();
+  pills[0].classList.add("active");
+  pills[4].classList.remove("active");
+  pills[1].classList.remove("active");
+  pills[2].classList.remove("active");
+  pills[3].classList.remove("active");
+  body.classList.add("ald");
+  body.classList.remove("h", "cor", "bes", "kam");
+  container.style.backgroundColor = "white";
 
-alderaan.addEventListener("click", function (e){
-	e.preventDefault();
-	body.classList.add("ald");
-	body.classList.remove("h", "cor", "bes", "kam");
-	fetch(rooturl)
-		.then(res => res.json())
-		.then(res => {
-			console.log(res.results[0])
-			description.innerText = `Welcome to ${res.results[0].name}! 
+  fetch(rooturl)
+    .then(res => res.json())
+    .then(res => {
+      console.log(res.results[0]);
+      description.innerText = `Welcome to ${res.results[0].name}! 
 			${res.results[0].name} is ${res.results[0].diameter} kilometers in diameter, and has ${res.results[0].orbital_period} days in a year.
 			The planet ${res.results[0].name} is ${res.results[0].surface_water}% water, and the terrain of ${res.results[0].terrain} inhabit ${res.results[0].population} beings.
 			Thanks for visiting and come back soon!`;
-		})
-		.catch(err=>{
-			console.log("try again!")
-		})
-})
-coruscant.addEventListener("click", function (e){
-	e.preventDefault();
-	body.classList.add("cor");
-	body.classList.remove("h", "ald", "bes", "kam")
-	fetch(rooturl)
-		.then(res => res.json())
-		.then(res => {
-			console.log(res.results[7])
-			description.innerText = `Welcome to ${res.results[7].name}! 
+    })
+    .catch(err => {
+      console.log("try again!");
+    });
+});
+coruscant.addEventListener("click", function(e) {
+  e.preventDefault();
+  pills[1].classList.add("active");
+  pills[0].classList.remove("active");
+  pills[4].classList.remove("active");
+  pills[2].classList.remove("active");
+  pills[3].classList.remove("active");
+  body.classList.add("cor");
+  body.classList.remove("h", "ald", "bes", "kam");
+  container.style.backgroundColor = "white";
+  fetch(rooturl)
+    .then(res => res.json())
+    .then(res => {
+      console.log(res.results[7]);
+      description.innerText = `Welcome to ${res.results[7].name}! 
 			${res.results[7].name} is ${res.results[7].diameter} kilometers in diameter, and has ${res.results[7].orbital_period} days in a year.
 			The planet ${res.results[7].name} is ${res.results[7].surface_water}% water, and the terrain of ${res.results[7].terrain} inhabit ${res.results[7].population} beings.
 			Thanks for visiting and come back soon!`;
-		})
-		.catch(err=>{
-			console.log("try again!")
-		})
-})
-hoth.addEventListener("click", function (e){
-	e.preventDefault();
-	body.classList.add("h");
-	body.classList.remove("ald", "cor", "bes", "kam")
-	fetch(rooturl)
-		.then(res => res.json())
-		.then(res => {
-			console.log(res.results[2])
-			description.innerText = `Welcome to ${res.results[2].name}! 
+    })
+    .catch(err => {
+      console.log("try again!");
+    });
+});
+hoth.addEventListener("click", function(e) {
+  e.preventDefault();
+  pills[2].classList.add("active");
+  pills[0].classList.remove("active");
+  pills[1].classList.remove("active");
+  pills[4].classList.remove("active");
+  pills[3].classList.remove("active");
+  body.classList.add("h");
+  body.classList.remove("ald", "cor", "bes", "kam");
+  container.style.backgroundColor = "white";
+  fetch(rooturl)
+    .then(res => res.json())
+    .then(res => {
+      console.log(res.results[2]);
+      description.innerText = `Welcome to ${res.results[2].name}! 
 			${res.results[2].name} is ${res.results[2].diameter} kilometers in diameter, and has ${res.results[2].orbital_period} days in a year.
 			The planet ${res.results[2].name} is ${res.results[2].surface_water}% water, and the terrain of ${res.results[2].terrain} inhabit ${res.results[2].population} beings.
 			Thanks for visiting and come back soon!`;
-		})
-		.catch(err=>{
-			console.log("try again!")
-		})
-})
-bespin.addEventListener("click", function (e){
-	e.preventDefault();
-	body.classList.add("bes");
-	body.classList.remove("h", "cor", "ald", "kam")
-	fetch(rooturl)
-		.then(res => res.json())
-		.then(res => {
-			console.log(res.results[4])
-			description.innerText = `Welcome to ${res.results[4].name}! 
+    })
+    .catch(err => {
+      console.log("try again!");
+    });
+});
+bespin.addEventListener("click", function(e) {
+  e.preventDefault();
+  pills[3].classList.add("active");
+  pills[0].classList.remove("active");
+  pills[1].classList.remove("active");
+  pills[2].classList.remove("active");
+  pills[4].classList.remove("active");
+  body.classList.add("bes");
+  body.classList.remove("h", "cor", "ald", "kam");
+  container.style.backgroundColor = "white";
+  fetch(rooturl)
+    .then(res => res.json())
+    .then(res => {
+      console.log(res.results[4]);
+      description.innerText = `Welcome to ${res.results[4].name}! 
 			${res.results[4].name} is ${res.results[4].diameter} kilometers in diameter, and has ${res.results[4].orbital_period} days in a year.
 			The planet ${res.results[4].name} is ${res.results[4].surface_water}% water, and the terrain of the ${res.results[4].terrain} inhabits ${res.results[4].population} beings.
 			Thanks for visiting and come back soon!`;
-		})
-		.catch(err=>{
-			console.log("try again!")
-		})
-})
-kamino.addEventListener("click", function (e){
-	e.preventDefault();
-	body.classList.add("kam");
-	body.classList.remove("h", "cor", "bes", "ald")
-	fetch(rooturl)
-		.then(res => res.json())
-		.then(res => {
-			console.log(res.results[8])
-			description.innerText = `Welcome to ${res.results[8].name}! 
+    })
+    .catch(err => {
+      console.log("try again!");
+    });
+});
+kamino.addEventListener("click", function(e) {
+  e.preventDefault();
+  pills[4].classList.add("active");
+  pills[0].classList.remove("active");
+  pills[1].classList.remove("active");
+  pills[2].classList.remove("active");
+  pills[3].classList.remove("active");
+  body.classList.add("kam");
+  body.classList.remove("h", "cor", "bes", "ald");
+  container.style.backgroundColor = "white";
+  fetch(rooturl)
+    .then(res => res.json())
+    .then(res => {
+      console.log(res.results[8]);
+      description.innerText = `Welcome to ${res.results[8].name}! 
 			${res.results[8].name} is ${res.results[8].diameter} kilometers in diameter, and has ${res.results[8].orbital_period} days in a year.
 			The planet ${res.results[8].name} is ${res.results[8].surface_water}% water, and the ${res.results[8].terrain} terrain inhabits ${res.results[8].population} beings.
 			Thanks for visiting and come back soon!`;
-		})
-		.catch(err=>{
-			console.log("try again!")
-		})
-})
+    })
+    .catch(err => {
+      console.log("try again!");
+    });
+});
